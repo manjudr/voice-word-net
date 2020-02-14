bab1cbe733743c7088c3c5099cc3227202b98ce9
Manjunath Davanam <manjunathd@ilimi.in>
2020-02-14 18:40:26 +0530

    Ekstep 2020 devcon voice wordnet project

--- scripts/mainController.js
+++ scripts/mainController.js
@@ -16,13 +16,19 @@ app.controller('myCtrl', function($scope, $rootScope, $http) {
     $scope.hide_text_area = true
     $scope.hide_nouns_tables = true
     $scope.hide_pre_positions_tables = true
-    $scope.nounsAre = ["smaple"]
-    $scope.prepositions = ["pre"]
+    $scope.hide_translated_tables = true
+    $scope.nounsAre = []
+    $scope.prepositions = []
     $scope.antonymsAre = []
     $scope.synonyms = []
     $scope.recorded_value = ""
     window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
     $scope.recognition = new window.SpeechRecognition();
+
+
+    $scope.translate_input_subwords = ""
+    $scope.translate_output_subwords = ""
+
 
     $scope.textToSpeech = function(message) {
         console.log("yes")
@@ -64,7 +70,7 @@ app.controller('myCtrl', function($scope, $rootScope, $http) {
                 "user-id": "ilimi",
                 "accept-encoding": "UTF-8",
                 "accept-charset": "UTF-8",
-                "authorization": "",
+                "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIyNDUzMTBhZTFlMzc0NzU1ODMxZTExZmQyMGRjMDg0MiIsImlhdCI6bnVsbCwiZXhwIjpudWxsLCJhdWQiOiIiLCJzdWIiOiIifQ.M1H_Z7WvwRPM0suBCofHs7iuDMMHyBjIRd3xGS4hqy8",
                 "cache-control": "no-cache",
                 "postman-token": "9e2d700f-b653-af17-58d9-0f926a9407aa"
             },
@@ -78,11 +84,9 @@ app.controller('myCtrl', function($scope, $rootScope, $http) {
     };
 
     $scope.startRecord = function() {
-        console.log("recording..")
         $scope.hide_wave_image = false
         $scope.hide_record_play_button = true;
         $scope.hide_record_stop_button = false;
-
         $scope.recognition.continuous = true;
         $scope.recognition.onresult = (event) => {
             for (var i = event.resultIndex; i < event.results.length; ++i) {
@@ -100,38 +104,74 @@ app.controller('myCtrl', function($scope, $rootScope, $http) {
     }
 
     $scope.stopRecord = function() {
-        $scope.recognition.stop();
         $scope.hide_wave_image = true
-        $scope.analyzeMessage($scope.recorded_value, function(response) {
-            console.log("response is" + JSON.stringify(response))
-            if (response.result.text_complexity.top5) {
-                $scope.nounsAre = response.result.text_complexity.top5.noun
-                $scope.prepositions = response.result.text_complexity.top5.preposition
-                if ($scope.nounsAre.length != 0) {
-                    $scope.hide_nouns_tables = false
+        if ($scope.recorded_value != undefined || $scope.recorded_value !== "") {
+            $scope.recognition.stop();
+            $scope.translate($scope.recorded_value, function(response) {
+                if (response.response_body != undefined) {
+                    $scope.hide_translated_tables = false
+                    $scope.translate_input_subwords = response.response_body[0].input_subwords
+                    $scope.translate_output_subwords = response.response_body[0].output_subwords
+                    $scope.safeApply()
                 }
-                if ($scope.prepositions.length != 0) {
-                    $scope.hide_pre_positions_tables = false
+            });
+            $scope.analyzeMessage($scope.recorded_value, function(response) {
+                console.log("response is" + JSON.stringify(response))
+                if (response.result.text_complexity.top5) {
+                    $scope.nounsAre = response.result.text_complexity.top5.noun
+                    $scope.prepositions = response.result.text_complexity.top5.preposition
+                    if ($scope.nounsAre.length != 0) {
+                        $scope.hide_nouns_tables = false
+                    }
+                    if ($scope.prepositions.length != 0) {
+                        $scope.hide_pre_positions_tables = false
+                    }
+                    console.log("Nouns List is " + JSON.stringify($scope.nounsAre))
+                } else {
+                    $scope.safeApply()
+                    alert("I'm Sorry.. I couldn't able to get the nouns for you, Please try again")
                 }
-                console.log("Nouns List is " + JSON.stringify($scope.nounsAre))
-                $scope.hide_record_play_button = false
-                $scope.hide_record_stop_button = true;
-                $scope.hide_record_play_button = true;
-                $scope.hide_second_teacher = false
-                $scope.hide_text_area = true
-                $scope.hide_recorded_text = false
-            } else {
-                $scope.hide_wave_image = false
-                $scope.safeApply()
-                alert("I'm Sorry.. I couldn't able to understand you, Please Speak Again")
-                $scope.recognition.start()
-
-            }
-
-        })
+            })
+            $scope.hide_record_stop_button = true;
+            $scope.hide_record_play_button = true;
+            $scope.hide_second_teacher = false
+            $scope.hide_text_area = true
+            $scope.hide_recorded_text = false
+            $scope.hide_wave_image = true
+        } else {
+            $scope.hide_wave_image = false
+            $scope.safeApply()
+            alert("I'm Sorry.. I couldn't able to understand you, Please Speak Again")
+        }
 
     }
 
+    $scope.translate = function(message, cb) {
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://50.1.0.11:3003/translator/translation_en",
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "User-Agent": "PostmanRuntime/7.19.0",
+                "Accept": "*/*",
+                "Cache-Control": "no-cache",
+                "Postman-Token": "0a357448-def3-47b2-beb2-c685d7ea60f1,bdbe7391-d38f-4087-a604-09a03a480fa0",
+                "Host": "50.1.0.11:3003",
+                "Accept-Encoding": "gzip, deflate",
+                "Content-Length": "63",
+                "Connection": "keep-alive",
+                "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": `[\n    {\n        \"src\": \"${message}\",\n        \"id\": 56\n    }\n]`
+        }
+        $.ajax(settings).done(function(response) {
+            console.log(response);
+            cb && cb(response)
+        });
+    }
     $scope.start = function() {
         $scope.hide_starting_page = true
         $scope.hide_talking_teacher = false
@@ -140,10 +180,5 @@ app.controller('myCtrl', function($scope, $rootScope, $http) {
                 //$scope.textToSpeech("Hello!!! Welcome to devcon event!!  Hope you have came here to learn about the antonyms and synonyms, Now I would request you to read something then i will find antonyms and synonyms for you!")
         }, 100)
     }
-
-
-
-
-
 
 });
